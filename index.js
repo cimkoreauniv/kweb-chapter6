d9a4255e35566b3ca5c082bc158b8baf5a358dae
cimkoreauniv <changilmoon@korea.ac.kr>
2021-11-09 22:20:23 +0900

    KWEB chapter 6

---
+++ index.js
@@ -0,0 +1,18 @@
+const util = require('util');
+const crypto = require('crypto');
+
+const pbkdf2 = util.promisify(crypto.pbkdf2);
+const randomBytes = util.promisify(crypto.randomBytes);
+
+const encrypt = async text => {
+    const ALGO = 'sha512';
+    const KEY_LEN = 64;
+    const salt = await randomBytes(32);
+    const iter = Math.floor(Math.random() * 20000) + 200000;
+    const digest = await pbkdf2(text, salt, iter, KEY_LEN, ALGO);
+    console.log(`${text} | ${salt.toString('base64')} ${digest.toString('base64')}`);
+};
+
+(async () => {
+    await encrypt('samplepassword');
+})();
